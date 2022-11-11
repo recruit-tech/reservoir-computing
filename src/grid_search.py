d295fc1f7f4437291c92cc212799c4f4415fb513
yamashin0922 <y@zenryokukikai.com>
2022-11-11 17:31:36 +0900

    Add num of cpu as args and show progress bar while waiting long time

--- src/grid_search.py
+++ src/grid_search.py
@@ -9,26 +9,23 @@ import batch_training_and_predict
 import time
 from multiprocessing import Pool
 
-cnt = 0
-num_grid_search = 0
+from tqdm import tqdm
 
 def wraper(parameters):
-    global cnt
-    global num_grid_search
-    cnt += 1
-    print(cnt, '/', num_grid_search)
+    #print(parameters.get_title_from_params())
     acc = batch_training_and_predict.main(APP, APP_NAME, parameters, params.csv_file, save_dir, is_show_chart=False)
+    print(acc, parameters.get_title_from_params())
     return acc, parameters.get_title_from_params()
 
 def main(app, app_name, parameters, csv_file, save_dir):
-    global num_grid_search
     parameters_list = []
-    while parameters.set_next_grid_search_params():
-        #parameters_list.append([app, app_name, copy.deepcopy(parameters), csv_file, save_dir])
-        parameters_list.append(copy.deepcopy(parameters))
+    print('Preparing a gird search. Please wait... ')
+    with tqdm() as pbar:
+        while parameters.set_next_grid_search_params():
+            parameters_list.append(copy.deepcopy(parameters))
+            pbar.update(1)
 
-    num_grid_search = len(parameters_list)
-    p = Pool(15) 
+    p = Pool(n_cpu) 
     accs = p.map(wraper, parameters_list)
     for result in sorted(accs, reverse=True):
         print(result)
@@ -54,6 +51,7 @@ if __name__ == '__main__':
     print('Application',APP)
 
     parser = argparse.ArgumentParser(description='Hyper parameter.')
+    parser.add_argument('-cpu', dest='cpu', type=int, default=15, help='num of cpu core')
     parser.add_argument('-csv_file', dest='csv_file', type=str, help='target data', required=True)
     parser.add_argument('-save_dir', dest='save_dir', default='output', type=str, help='Directory for output files')
 
@@ -71,6 +69,8 @@ if __name__ == '__main__':
 
     # Make directory if not exist
     os.makedirs(save_dir, exist_ok=True)
+
+    n_cpu = params.cpu
 
     try:
         main(APP, APP_NAME, parameters, params.csv_file, save_dir)
