9e840226f5aa43f652e3d0b8f176666763f52292
yamamoto_shinya <y@zenryokukikai.com>
2022-11-11 23:12:49 +0900

    Fix the bug of echo to .env

--- src/test.sh
+++ src/test.sh
@@ -1,6 +1,6 @@
 mv .env .env.bk
-echo APPLICATION_NAME=app_thumbup >> .env
+echo APPLICATION_NAME=app_thumbup > .env
 python batch_training_and_predict.py -csv_file output/train_log_20221101_115348.csv
-echo APPLICATION_NAME=app_thumbup_new >> .env
+echo APPLICATION_NAME=app_thumbup_new > .env
 python grid_search.py -csv_file output/thumbup_nc_data.csv -cpu 4
 mv .env.bk .env
