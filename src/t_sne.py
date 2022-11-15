5da259f7f9f167de0cea309dce3977305b1add27
yamashin0922 <y@zenryokukikai.com>
2022-11-16 00:16:22 +0900

    Add hovering on a plot

--- src/t_sne.py
+++ src/t_sne.py
@@ -10,64 +10,49 @@ from sklearn.manifold import TSNE
 from matplotlib.colors import Normalize
 
 
-def normalize(v, axis=-1, order=2):
-    l2 = np.linalg.norm(v, ord = order, axis=axis, keepdims=True)
-    l2[l2==0] = 1
-    return v/l2
+def update_annot(ind):
+    cmap = plt.cm.jet
 
-def min_max(x, axis=None):
-    min = x.min(axis=axis, keepdims=True)
-    max = x.max(axis=axis, keepdims=True)
-    result = (x-min)/(max-min)
-    return result
+    pos = sc.get_offsets()[ind["ind"][0]]
+    annot.xy = pos
+    #print([org_parameters[n][:] for n in ind["ind"]])
+    #print([org_accuracy[n] for n in ind["ind"]])
+    text = ''
+    for acc, params in [(org_accuracy[n], org_parameters[n][:]) for n in ind["ind"]]:
+        print(acc, params)
+        text += str(acc) + ': ' + ','.join([str(i) for i in params]) + '\n'
+    print(text)
+    annot.set_text(text)
 
-def zscore(x, axis = None):
-    xmean = x.mean(axis=axis, keepdims=True)
-    xstd  = np.std(x, axis=axis, keepdims=True)
-    zscore = (x-xmean)/xstd
-    return zscore
 
+def hover(event):
+    vis = annot.get_visible()
+    if event.inaxes == ax:
+        cont, ind = sc.contains(event)
+        if cont:
+            update_annot(ind)
+            annot.set_visible(True)
+            fig.canvas.draw_idle()
+        else:
+            if vis:
+                annot.set_visible(False)
+                fig.canvas.draw_idle()
 
 def read_acc_and_params_from_csv(csv_file):
     df = pd.read_csv(csv_file)
     df = df.fillna(0)
 
+    org_accs = df['acc']
     accs = np.array(df['acc'] * 100, dtype='int64')
 
     df = df[df.columns[df.columns != 'acc']]
-    df = df[df.columns[df.columns != 'fb_scale']]
-    params = (df - df.min()) / (df.max() - df.min())
+    org_params = df[df.columns[df.columns != 'fb_scale']]
+    params = (org_params - org_params.min()) / (org_params.max() - org_params.min())
 
-    print(accs)
-    print(params)
-    return accs, params.to_numpy()
+    #print(accs)
+    #print(params)
+    return accs, params.to_numpy(), org_accs, org_params.to_numpy()
 
-
-def main(csv_file):
-
-    accuracy, parameters = read_acc_and_params_from_csv(csv_file)
-
-    print(parameters)
-    print(type(parameters))
-
-    norm = Normalize(vmin=0.95*min(accuracy), vmax=1.05*max(accuracy))
-
-
-    parameters3d = TSNE(n_components=3, random_state=1).fit_transform(parameters)
-
-    from mpl_toolkits.mplot3d import Axes3D
-
-    fig = plt.figure(figsize=(10, 10)).gca(projection='3d')
-
-    max_acc = np.max(accuracy)
-    for i in np.unique(accuracy):
-        target = parameters3d[accuracy == i]
-        if i != max_acc:
-            fig.scatter(target[:, 0], target[:, 1], target[:, 2], c=accuracy[accuracy == i], norm=norm, cmap='jet', marker='.', alpha=0.5)
-        else:
-            fig.scatter(target[:, 0], target[:, 1], target[:, 2], s=100, c='black', marker='^')
-
-    plt.show()
 
 if __name__=="__main__":
 
@@ -77,8 +62,30 @@ if __name__=="__main__":
     params = parser.parse_args()
     csv_file = params.csv_file
 
-    try:
-        main(csv_file)
-    except KeyboardInterrupt:
-        print('closeing...')
-    exit()
+    accuracy, parameters, org_accuracy, org_parameters = read_acc_and_params_from_csv(csv_file)
+
+    #print(parameters)
+    #print(type(parameters))
+
+    norm = Normalize(vmin=0.95*min(accuracy), vmax=1.05*max(accuracy))
+
+
+    parameters3d = TSNE(n_components=3, random_state=1).fit_transform(parameters)
+
+    from mpl_toolkits.mplot3d import Axes3D
+
+    fig = plt.figure(figsize=(10, 10))
+    ax = fig.add_subplot(projection='3d')
+
+    sc = ax.scatter(parameters3d[:, 0], parameters3d[:, 1], parameters3d[:, 2], c=accuracy, norm=norm, cmap='jet', marker='.', alpha=0.5)
+
+    fig.colorbar(sc, ax=ax, location='left')
+
+    annot = ax.annotate("", xy=(0,0), xytext=(20,20),textcoords="offset points",
+                    bbox=dict(boxstyle="round", fc="w"),
+                    arrowprops=dict(arrowstyle="->"))
+    
+    fig.canvas.mpl_connect("motion_notify_event", hover)
+
+    plt.show()
+
